5aa5f46ee465c2b83b3caaea7d26326389fc7aeb
Will Hoback <will.hoback@gmail.com>
2019-12-25 00:24:29 -0500

    add paddles

--- main.lua
+++ main.lua
@@ -30,8 +30,12 @@ function love.draw()
     push:apply('start')
     -- change bg color using RGBA colors
     love.graphics.clear(R, G, B, A)
-    -- draw rect for pong ball
+    -- draw rect for pong ball      
     love.graphics.rectangle('fill', V_WIDTH/2, V_HEIGHT/2, 5, 5)
+
+    -- draw paddles on both sides
+    love.graphics.rectangle('fill', 5, 20, 5, 20)
+    love.graphics.rectangle('fill', V_WIDTH-10,V_HEIGHT-40, 5, 20)
     love.graphics.printf(
         "Hello Pong", 
         0, 
